4db11df71610fa02cb92fde35565e3bd7efe43f6
Lawrence Ime <edetime94@gmail.com>
2024-05-23 08:01:06 +0100

    I tweaked the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        referrerPolicy: 'origin-when-cross-origin',
-      },
+    async headers() {
+        return [
+            {
+                source: '/(.*)', // This applies the header to all routes
+                headers: [
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin', // Set the referrer policy you need
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
-

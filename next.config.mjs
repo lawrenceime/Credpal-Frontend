bd840f18acab7e9c7643332f1b93d7f05ecb5c54
Lawrence Ime <edetime94@gmail.com>
2024-05-23 07:42:53 +0100

    I changed next.config.mjs configuraton inorder to aid vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        referrerPolicy: 'origin-when-cross-origin',
+      },
+};
 
 export default nextConfig;
+
